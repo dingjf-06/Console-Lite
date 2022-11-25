8f3c617bcbf5dd2b8528012f39540ded9d987eca
dingjf-06 <93962471+dingjf-06@users.noreply.github.com>
2022-11-25 17:20:24 +0800

    Fixed bug when copying seats from different interface

--- controller/views/seats/seats.js
+++ controller/views/seats/seats.js
@@ -19,7 +19,9 @@ const SeatsView = Vue.extend({
     },
 
     performEditing() {
-      const str = this.$refs.seatsInput.innerHTML;
+      const rstr = this.$refs.seatsInput.innerHTML;
+      const str = rstr.replace(/<div>/g, '').replace(/<\/div>/g, '<br>')
+      console.log(str);
       const seats = str.split('<br>')
         .filter(e => e.length > 0)
         .map(e => ({ name: e, present: false }));
